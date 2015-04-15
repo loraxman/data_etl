a21e30b3db8fbf7db5989b51bd1054a7e4b6f1ca
Roger Kelly <loraxman@gmail.com>
2015-04-15 09:38:22 -0500

    small stuff

--- app/etl/claimload.py
+++ app/etl/claimload.py
@@ -3,10 +3,14 @@ import s3util
 
 def execstep(queue=None,step=None,step_pids=None):
 	step.active=True
+	
 	for wstep in step.wait_steps:
 		for wpid in step_pids[wstep]:
 			wpid.join()
 
+
+    conn = psycopg2.connect(step.connectdb)
+    cur = conn.cursor()
 
 	s3util.get_s3_file("00",named_pipe="claimraw.dat")	
 	sq = StepQueueEntry (step,"PASS")

d32221dd4e2116195eb9c92b5bcde6c463631461
Roger Kelly <loraxman@gmail.com>
2015-07-28 15:08:08 -0500

    member digest load

--- app/etl/clear_keenstage.sql
+++ app/etl/clear_keenstage.sql
@@ -5,5 +5,6 @@ truncate  staging.keen_json_signed_up ;
 truncate  staging.keen_json_signed_in ;
 truncate  staging.keen_json_user ;
 truncate  staging.keen_json_user_completes_provider_review ;
+truncate  staging.wm_digest_to_member_id_mapping;
 commit;
 select 'PASS';

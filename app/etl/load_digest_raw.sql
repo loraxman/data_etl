02869dda56f75d6074c780052548da4a59b90044
Roger Kelly <loraxman@gmail.com>
2015-07-28 15:38:32 -0500

    path for file

--- app/etl/load_digest_raw.sql
+++ app/etl/load_digest_raw.sql
@@ -1,6 +1,6 @@
 set client_encoding to 'latin1';
 
-COPY staging.wm_digest_to_member_id_mapping FROM '/home/ubuntu/data_projects/data_etl/digest_to_member_id_mapping.txt' delimiter ',';
+COPY staging.wm_digest_to_member_id_mapping FROM '/home/ubuntu/data_projects/data_etl/app/etl/digest_to_member_id_mapping.txt' delimiter ',';
 
 commit;
 

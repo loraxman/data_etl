07b778d6decd5e58e7909e1998748cbb54e13caa
Roger Kelly <loraxman@gmail.com>
2015-07-28 15:02:59 -0500

    adding raw digest

---
+++ app/etl/load_digest_raw.sql
@@ -0,0 +1,6 @@
+set client_encoding to 'latin1';
+
+COPY staging.wm_digest_to_member_id_mapping FROM '/home/ubuntu/data_projects/data_etl/digest_to_member_id_mapping.txt' delimiter ',';
+
+commit;
+

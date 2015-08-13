87ccce2d70cceb979bc432504dff7f04f69beb55
Roger Kelly <loraxman@gmail.com>
2015-08-13 16:16:27 -0500

    removing keen json load;

--- app/etl/clear_keenstage.sql
+++ app/etl/clear_keenstage.sql
@@ -1,10 +1,10 @@
 
 
-truncate  staging.keen_json_procedure_searched ;
-truncate  staging.keen_json_signed_up ;
-truncate  staging.keen_json_signed_in ;
-truncate  staging.keen_json_user ;
-truncate  staging.keen_json_user_completes_provider_review ;
+--truncate  staging.keen_json_procedure_searched ;
+--truncate  staging.keen_json_signed_up ;
+--truncate  staging.keen_json_signed_in ;
+--truncate  staging.keen_json_user ;
+--truncate  staging.keen_json_user_completes_provider_review ;
 truncate  staging.wm_digest_to_member_id_mapping;
 commit;
 select 'PASS';
